ce301b1dc623379543ab574bcb85aaa08e35d761
Tanvir Hossen Bappy <tanvirbappy7706@gmail.com>
2023-07-23 14:03:38 +0600

    edit splade form

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -7,6 +7,9 @@ use App\Tables\Country;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use ProtoneMedia\Splade\Facades\Splade;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use ProtoneMedia\Splade\SpladeForm;
 
 class CountryController extends Controller
 {
@@ -56,7 +59,21 @@ class CountryController extends Controller
      */
     public function edit(\App\Models\Country $country): View
     {
-        return view('admin.countries.edit', compact('country'));
+        $form = SpladeForm::make()
+            ->action(route('admin.countries.update', $country))
+            ->fields([
+                Input::make('country_code')->label('Country Code'),
+                Input::make('name')->label('User Name'),
+                Submit::make()->label('Update')->class('text-white mt-5 bg-green-500'),
+            ])
+            ->fill($country)
+            ->class('space-y-4 bg-white rounded p-4')
+            ->method('PUT');
+
+        return view('admin.countries.edit', [
+            'form' => $form,
+            'country' => $country
+        ]);
     }
 
     /**
---
+++ resources/views/admin/countries/edit.blade.php
@@ -0,0 +1,7 @@
+<x-admin-layout>
+    <h1 class="mt-4 mx-auto font-bold text-2xl monospace text-emerald-700">Edit Country</h1>
+
+    <div class="mt-5 pb-3">
+        <x-splade-form :for="$form"/>
+    </div>
+</x-admin-layout>
